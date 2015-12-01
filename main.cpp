e1b6c6148bc5d10b11e8a075d922fc389d0212b8
YunJongSeob <cjsgkwkd4@gmail.com>
2015-12-01 18:12:43 +0900

    Histogram Equalization 추가
    추가내용은 아직 구현 안 함.

--- main.cpp
+++ main.cpp
@@ -32,6 +32,7 @@ const char OutputFileName[6][50]  = {"video_none.avi" ,				// output file
 
 VideoCapture * global_cap;
 
+// Video Processing
 enum {VP_NONE, VP_HIST_EQUAL, VP_HIST_STRETCH, VP_GAMMA, VP_UNSHARP, VP_ACHROMATIC};
 
 void Menu();
@@ -42,6 +43,9 @@ void onGammaChangeSlide(int pos, void * userdata = 0); /* pos means trackbar pos
 void onUnsharpSlide(int pos, void * userdata = 0); /* pos means trackbar position. It means threshold value here. */;
 
 void CreateWindowAndTrackbar(Mat trackbar_background, const char windowName[], const char trackbarName[], int * default, int max, void (*trackbarCallBack)(int pos, void * userdata) );
+
+Mat GetHistEqualOnColorImg(const Mat &src);
+Mat GetUnsharpImg(const Mat &src);
 
 int main(void) 
 {
@@ -143,9 +147,9 @@ int main(void)
 	int sigma_default = 0;
 	int sigma_max = 10;
 	int sigma = sigma_default;
-				
+	
 	Menu();
-
+		
 	while(1)
 	{	
 		if (video_play == true)
@@ -177,12 +181,14 @@ int main(void)
 
 			switch (video_proc)
 			{
-			case VP_HIST_EQUAL   : break;
+			case VP_HIST_EQUAL   : frame = GetHistEqualOnColorImg(frame);
+								   break;
 			case VP_HIST_STRETCH : break;
 			case VP_GAMMA		 : break;
 			case VP_UNSHARP		 : break;
 			case VP_ACHROMATIC   : break;
-			default				 : break;
+			default				 :  
+								   break;
 			}
 			
 			if(video_save_flag == true)
@@ -235,8 +241,10 @@ int main(void)
 					break;		
 		case '2'  : video_proc = VP_HIST_STRETCH;	// 히스토그램 스트레칭
 					cout << "Histogram Stretching " << endl;
-					CreateWindowAndTrackbar(trackbar_background, histStretchWindowName, histStretchTrackbarName[0], &histStretch_default[0], histStretch_max, onHistStretchMinSlide);
-					CreateWindowAndTrackbar(trackbar_background, histStretchWindowName, histStretchTrackbarName[1], &histStretch_default[1], histStretch_max, onHistStretchMaxSlide);
+					CreateWindowAndTrackbar(trackbar_background, histStretchWindowName, histStretchTrackbarName[0], 
+														&histStretch_default[0], histStretch_max, onHistStretchMinSlide);
+					CreateWindowAndTrackbar(trackbar_background, histStretchWindowName, histStretchTrackbarName[1], 
+														&histStretch_default[1], histStretch_max, onHistStretchMaxSlide);
 					break;		
 		case '3'  : video_proc = VP_GAMMA;			// 감마 변환
 					cout << "Change Gamma " << endl;
@@ -312,4 +320,82 @@ void CreateWindowAndTrackbar( Mat trackbar_background, const char windowName[], 
 	namedWindow(windowName);
 	imshow(windowName, trackbar_background);
 	createTrackbar(trackbarName, windowName, default, max, trackbarCallBack);
+}
+
+cv::Mat GetHistEqualOnColorImg( const Mat &src )
+{
+	cv::Mat dst, gray, tmp;
+	cv::Mat Lin, Lout, Cin, Cout;  
+	cv::Mat vCin[3], vCout[3];     // vectorCin/out
+
+	// Convert to gray and Apply Histogram Equalization
+	cvtColor(src, gray, CV_BGR2GRAY);
+	equalizeHist(gray, Lout);
+
+	src.convertTo(Cin, CV_32FC3);
+	cv::split(Cin, vCin);					// vCin[0] = B, vCin[1] = G, vCin[0] = R, 
+
+	gray.convertTo(Lin,CV_32FC1);			// 32 bit data
+	Lout.convertTo(Lout,CV_32FC1);			// 32 bit data
+
+	// 아래 연산은 안되는 듯함. runtime 오류 *** 검토 필요. 
+	//vCout[0] = Lout * (vCin[0]/Lin); 				//vCout[1] = Lout * (vCin[1]/Lin);				//vCout[2] = Lout * (vCin[2]/Lin);
+
+	//double gamma = 0.01;			// 흑백으로 칼라 강도가 약해짐.
+	//double gamma = 5.01;			// 칼라 강도가 매우 강해짐.
+	//double gamma = 1.5;				// 선명한 칼라.
+	//double gamma = 0.6;				// 약한 칼라
+	double gamma = 1.0;				// No strength control
+	cv::divide(vCin[0], Lin, tmp);		cv::pow(tmp, gamma, tmp); cv::multiply(Lout, tmp, vCout[0]);
+	cv::divide(vCin[1], Lin, tmp);		cv::pow(tmp, gamma, tmp); cv::multiply(Lout, tmp, vCout[1]);
+	cv::divide(vCin[2], Lin, tmp);		cv::pow(tmp, gamma, tmp); cv::multiply(Lout, tmp, vCout[2]);
+
+	cv::merge(vCout, 3, dst);			// Merge 3 planes to a Mat.
+	dst.convertTo(dst,CV_8UC3);
+
+	return dst;
+}
+
+cv::Mat GetUnsharpImg( const Mat &src )
+{
+	// Initialize common variables and objects over the test method sections.
+	int ksize = 31;					// Kernel Size. must be odd positive.  Big kernel size causes no problem for simulation. It affects on time during real time implementation.
+	float	sigma = 12.0;			// Test for the various sigmas, eg. 3, 7, ... etc. Can you tell the effect of the magnitude of sigma? It affects on local or global aspect of emphasis.
+	float	scale = 2.0;			// Test for the various scales, eg. 3, 7, etc.  Can you tell the effect of scale? It affects on the strength of emphasis.
+
+	cv::Mat Blur, dst, dstPositive, dstNegative;
+	cv::GaussianBlur(src, Blur, cv::Size(ksize, ksize), sigma);		// kernel size = ksize
+	cv::namedWindow("GaussianBlur");		cv::imshow("GaussianBlur", Blur); 
+
+	src.convertTo(src,CV_16S);
+	Blur.convertTo(Blur, CV_16S);
+
+	//1) Show high frequence components, Unsharp mask
+	// Unsharp mask = original image - Bluurred image
+	// The unsharp mask is identified later as a negative 2nd derivative of the origonal image.
+	dst = scale * (src-Blur);						// Unsharp mask
+	dst.convertTo(dstPositive,CV_8U,1);
+	dst.convertTo(dstNegative,CV_8U,-1);			// dstNegative contains Negative part of dst.
+
+	//cv::namedWindow("Positive Part of Unsharp Mask(original-Blur)"); 	cv::imshow("Positive Part of Unsharp Mask(original-Blur)",dstPositive);
+	//cv::namedWindow("Negative Part of Unsharp Mask(original-Blur)"); 	cv::imshow("Negative Part of Unsharp Mask(original-Blur)",dstNegative);
+
+
+	// 2) The unsharp mask is (original-Blur). Add it to original image.
+	dst = src + scale * (src-Blur);
+	dst.convertTo(dstPositive,CV_8U,1);
+	dst.convertTo(dstNegative,CV_8U,-1);			// dstNegative contains Negative part of dst.
+
+	//cv::namedWindow("Positive: original+UnsharpMask(original-Blur)"); 	cv::imshow("Positive: original+UnsharpMask(original-Blur)",dstPositive);
+	//cv::namedWindow("Negative: original+UnsharpMask(original-Blur)"); 	cv::imshow("Negative: original+UnsharpMask(original-Blur)",dstNegative);
+
+	// 3) If we subtract the 2nd derivative from the original image, the output image is sharpened. 
+	// Through trial and error, I found that the (Blur-original) is the 2nd derivative of the original image.
+	dst = src - scale * (Blur-src);
+	dst.convertTo(dstPositive,CV_8U,1);
+	dst.convertTo(dstNegative,CV_8U,-1);			// dstNegative contains Negative part of dst.
+
+	//cv::namedWindow("Positive: original-2ndDerivative(Blur-original)"); 	cv::imshow("Positive: original-2ndDerivative(Blur-original)",dstPositive);
+	//cv::namedWindow("Negative: original-2ndDerivative(Blur-original)"); 	cv::imshow("Negative: original-2ndDerivative(Blur-original)",dstNegative);
+	return dst;	
 }
