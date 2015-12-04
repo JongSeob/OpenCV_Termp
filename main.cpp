5a7fbdf3d38b8a5e0d5e23a58aaa349980a8f924
YunJongSeob <cjsgkwkd4@gmail.com>
2015-12-04 19:55:23 +0900

    감마관련정보 txt파일 추가 , hue를 움직이는 동작이 제대로 동작하지 않음

--- main.cpp
+++ main.cpp
@@ -10,11 +10,12 @@
 
 #include "common.h"
 #include "func.h"
+#include "img_proc.h"
 
 #define OUTPUT_PATH "E:/dip_termp/"
 
 char InputFilePath[100]  = "d:/dip/Images/";					// Specify the directory for the file to be saved in
-const char InputFileName[50]   = "the_return_of_the_king.avi";	// input video file
+const char InputFileName[50]   = "bike.avi";//"the_return_of_the_king.avi";	// input video file
 
 char OutputFilePath[6][100] = {OUTPUT_PATH ,				// output file
 	OUTPUT_PATH ,
@@ -33,12 +34,6 @@ const char OutputFileName[6][50]  = {"video_none.avi" ,				// output file
 VideoCapture * global_cap;
 
 void Menu();
-
-Mat GetHistEqualOnColorImg(const Mat &src);
-Mat GetUnsharpImg(const Mat &src);
-Mat GetHistStretch(const Mat &src);
-
-void Imadjust(const Mat &input, Mat &output, const double input_range[], const double output_range, const double gamma);
 
 int main(void) 
 {
@@ -83,8 +78,7 @@ int main(void)
 	// Set the mouse event callback function
 
 	setMouseCallback("original", mouse_callback);
-
-
+	
 	//////////////////////////////////////////////////////////////////////////
 	// 4. Open The File and Define Video Information Values
 	//////////////////////////////////////////////////////////////////////////
@@ -172,21 +166,22 @@ int main(void)
 			switch (video_proc)
 			{
 			case VP_HIST_EQUAL   : frame = GetHistEqualOnColorImg(frame);
-				break;
+							       break;
 			case VP_HIST_STRETCH : frame = GetHistStretch(frame);
-				break;
-			case VP_GAMMA		 : break;
+				                   break;
+			case VP_GAMMA		 : frame = GetGammaChangedImg(frame);
+				                   break;
 			case VP_UNSHARP		 : frame = GetUnsharpImg(frame);
-				break;
-			case VP_ACHROMATIC   : break;
-			default				 :  
-				break;
+				                   break;
+			case VP_ACHROMATIC   : frame = GetAchromaticImg(frame);
+								   break;
+			default				 : break;
 			}
 
 			if(video_save_flag == true)
 				wrt.write(frame);
 
-			runningTime = cap.get(CV_CAP_PROP_POS_MSEC);
+			runningTime = (int)cap.get(CV_CAP_PROP_POS_MSEC);
 
 			slider_position = runningTime;
 
@@ -198,12 +193,12 @@ int main(void)
 
 		int key;
 
-		key = waitKey(1000.0/(fps*speed));	// 동영상의 fps와 비슷한 속도로 영상을 출력하기 위해 읽는 속도를 조절.
+		key = (int)waitKey(1000.0/(fps*speed));	// 동영상의 fps와 비슷한 속도로 영상을 출력하기 위해 읽는 속도를 조절.
 
 		if(key == 0x1b)
 			break;
 
-		
+
 
 		// 중복선택 된 경우 처음으로 되돌아감
 		if( (key - '0') == video_proc )
@@ -272,110 +267,4 @@ void Menu()
 	cout << "==    's'    : video write start / stop        ==" << endl;
 	cout << "== SPACE_BAR : video pause / resume            ==" << endl;
 	cout << "=================================================" << endl;
-}
-
-cv::Mat GetHistEqualOnColorImg( const Mat &src )
-{
-	cv::Mat dst, gray, tmp;
-	cv::Mat Lin, Lout, Cin, Cout;  
-	cv::Mat vCin[3], vCout[3];     // vectorCin/out
-
-	// Convert to gray and Apply Histogram Equalization
-	cvtColor(src, gray, CV_BGR2GRAY);
-	equalizeHist(gray, Lout);
-
-	src.convertTo(Cin, CV_32FC3);
-	cv::split(Cin, vCin);					// vCin[0] = B, vCin[1] = G, vCin[0] = R, 
-
-	gray.convertTo(Lin,CV_32FC1);			// 32 bit data
-	Lout.convertTo(Lout,CV_32FC1);			// 32 bit data
-
-	// 아래 연산은 안되는 듯함. runtime 오류 *** 검토 필요. 
-	//vCout[0] = Lout * (vCin[0]/Lin); 				//vCout[1] = Lout * (vCin[1]/Lin);				//vCout[2] = Lout * (vCin[2]/Lin);
-
-	//double gamma = 0.01;			// 흑백으로 칼라 강도가 약해짐.
-	//double gamma = 5.01;			// 칼라 강도가 매우 강해짐.
-	//double gamma = 1.5;				// 선명한 칼라.
-	//double gamma = 0.6;				// 약한 칼라
-	double gamma = 1.0;				// No strength control
-	cv::divide(vCin[0], Lin, tmp);		cv::pow(tmp, gamma, tmp); cv::multiply(Lout, tmp, vCout[0]);
-	cv::divide(vCin[1], Lin, tmp);		cv::pow(tmp, gamma, tmp); cv::multiply(Lout, tmp, vCout[1]);
-	cv::divide(vCin[2], Lin, tmp);		cv::pow(tmp, gamma, tmp); cv::multiply(Lout, tmp, vCout[2]);
-
-	cv::merge(vCout, 3, dst);			// Merge 3 planes to a Mat.
-	dst.convertTo(dst,CV_8UC3);
-
-	return dst;
-}
-
-cv::Mat GetUnsharpImg( const Mat &src )
-{
-	Mat img = src;
-	// Initialize common variables and objects over the test method sections.
-	int ksize = 31;					// Kernel Size. must be odd positive.  Big kernel size causes no problem for simulation. It affects on time during real time implementation.
-	float	sigma = 12.0;			// Test for the various sigmas, eg. 3, 7, ... etc. Can you tell the effect of the magnitude of sigma? It affects on local or global aspect of emphasis.
-	float	scale = 2.0;			// Test for the various scales, eg. 3, 7, etc.  Can you tell the effect of scale? It affects on the strength of emphasis.
-
-	cv::Mat Blur, dst, dstPositive, dstNegative;
-	cv::GaussianBlur(img, Blur, cv::Size(ksize, ksize), sigma);		// kernel size = ksize
-	//cv::namedWindow("GaussianBlur");		cv::imshow("GaussianBlur", Blur); 
-
-	img.convertTo(img,CV_16S);
-	Blur.convertTo(Blur, CV_16S);
-
-#if 0
-	//1) Show high frequence components, Unsharp mask
-	// Unsharp mask = original image - Bluurred image
-	// The unsharp mask is identified later as a negative 2nd derivative of the origonal image.
-	dst = scale * (src-Blur);						// Unsharp mask
-	dst.convertTo(dstPositive,CV_8U,1);
-	dst.convertTo(dstNegative,CV_8U,-1);			// dstNegative contains Negative part of dst.
-
-	cv::namedWindow("Positive Part of Unsharp Mask(original-Blur)"); 	cv::imshow("Positive Part of Unsharp Mask(original-Blur)",dstPositive);
-	cv::namedWindow("Negative Part of Unsharp Mask(original-Blur)"); 	cv::imshow("Negative Part of Unsharp Mask(original-Blur)",dstNegative);
-#endif
-
-
-	// 2) The unsharp mask is (original-Blur). Add it to original image.
-	dst = img + scale * (img-Blur);
-	dst.convertTo(dstPositive,CV_8U,1);
-	dst.convertTo(dstNegative,CV_8U,-1);			// dstNegative contains Negative part of dst.
-
-	//cv::namedWindow("Positive: original+UnsharpMask(original-Blur)"); 	cv::imshow("Positive: original+UnsharpMask(original-Blur)",dstPositive);
-	//cv::namedWindow("Negative: original+UnsharpMask(original-Blur)"); 	cv::imshow("Negative: original+UnsharpMask(original-Blur)",dstNegative);
-
-#if 0
-	// 3) If we subtract the 2nd derivative from the original image, the output image is sharpened. 
-	// Through trial and error, I found that the (Blur-original) is the 2nd derivative of the original image.
-	dst = src - scale * (Blur-src);
-	dst.convertTo(dstPositive,CV_8U,1);
-	dst.convertTo(dstNegative,CV_8U,-1);			// dstNegative contains Negative part of dst.
-
-	cv::namedWindow("Positive: original-2ndDerivative(Blur-original)"); 	cv::imshow("Positive: original-2ndDerivative(Blur-original)",dstPositive);
-	cv::namedWindow("Negative: original-2ndDerivative(Blur-original)"); 	cv::imshow("Negative: original-2ndDerivative(Blur-original)",dstNegative);
-#endif
-
-	return dstPositive;	
-}
-
-cv::Mat GetHistStretch( const Mat &src )
-{
-	Mat img = src;
-
-	// Make a table for Histogram streteching
-	int	min=30, max=180;
-	int		 i,j;
-	unsigned char LUT[256];
-	for ( i=0; i<min; i++)		LUT[i] = 0;
-	for ( i=max; i<=255; i++)	LUT[i] = 255;	
-	for ( i=min; i<max; i++)	{	LUT[i] = (unsigned char) ( (float)i * 255.0 / (float)(max-min) - (255.0 * min / (float(max-min) ) ) );}
-
-	for (j =0; j<img.rows; j++) 
-		for (i =0; i<img.cols; i++) {
-			img.at<cv::Vec3b>(j,i)[0] = LUT[ img.at<cv::Vec3b>(j,i)[0] ];	
-			img.at<cv::Vec3b>(j,i)[1] = LUT[ img.at<cv::Vec3b>(j,i)[1] ];	
-			img.at<cv::Vec3b>(j,i)[2] = LUT[ img.at<cv::Vec3b>(j,i)[2] ];	
-		}
-
-		return img;
 }
